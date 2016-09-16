4ddb74d66fbd6f33dafc26367fb8afb62621e30e
Ivan Vokhmin <Cabalbl4@users.noreply.github.com>
2016-09-16 14:34:23 +0300

    Hide inside context

--- namespace.js
+++ namespace.js
@@ -1,28 +1,30 @@
-var __namespace__ = this;
-Object.defineProperty(this, "_namespace",{
-  get: function() {
-    return __namespace__;
-  },
-  set: function(_ns) {
-    var current = null;
-    if(typeof _ns == "string") {
-          if(_ns.indexOf(".") == 0) {
-            current = __namespace__;
-            _ns = _ns.substr(1);
-          } else {
-            current = this;
-          }
-      
-          var nsArr = _ns.split(".");
-          for(var i=0; i<nsArr.length; i++) {
-            if(!current[nsArr[i]])
-              current[nsArr[i]] = {};
-              current =  current[nsArr[i]];
-          };
+!function(_context) {
+var __namespace__ = _context;
+    Object.defineProperty(_context, "_namespace",{
+      get: function() {
+        return __namespace__;
+      },
+      set: function(_ns) {
+        var current = null;
+        if(typeof _ns == "string") {
+              if(_ns.indexOf(".") == 0) {
+                current = __namespace__;
+                _ns = _ns.substr(1);
+              } else {
+                current = _context;
+              }
+          
+              var nsArr = _ns.split(".");
+              for(var i=0; i<nsArr.length; i++) {
+                if(!current[nsArr[i]])
+                  current[nsArr[i]] = {};
+                  current =  current[nsArr[i]];
+              };
 
-    } else {
-      current = _ns;
-    }
-    __namespace__ = current;
-  }
-});
+        } else {
+          current = _ns;
+        }
+        __namespace__ = current;
+      }
+    });
+}(this);
